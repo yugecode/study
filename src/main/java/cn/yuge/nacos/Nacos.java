bd70c0172b575ec3e1484600cca3a496f468cd6a
luojiayu <luojiayu@ghostcloud.cn>
2020-11-09 10:52:14 +0800

    添加nacos

--- src/main/java/cn/yuge/nacos/Nacos.java
+++ src/main/java/cn/yuge/nacos/Nacos.java
@@ -2,12 +2,15 @@ package cn.yuge.nacos;
 
 import com.alibaba.cloud.nacos.discovery.NacosDiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,6 +20,9 @@ import java.util.List;
 @RestController
 public class Nacos {
 
+    @Value("${spring.application.name}")
+    private String name;
+
     @Resource
     private NacosDiscoveryClient nacosDiscoveryClient;
 
@@ -25,12 +31,17 @@ public class Nacos {
 
     @GetMapping("/test")
     public List<String> test() {
-        for(String s :nacosDiscoveryClient.getServices()){
+        List<String> slist = new ArrayList<>();
+        for (String s : nacosDiscoveryClient.getServices()) {
+            if (s.equals(name)) {
+                continue;
+            }
             ServiceInstance choose = loadBalancerClient.choose(s);
-            System.out.println(choose.getUri());
-
-
+            String url = choose.getUri() + "/test/getAllURL";
+            RestTemplate restTemplate = new RestTemplate();
+            String result = restTemplate.getForObject(url, String.class);
+            slist.add(s + "::" + result);
         }
-        return nacosDiscoveryClient.getServices();
+        return slist;
     }
 }
